a358e40bd365c44a50e5080b9c4a6d5032dce837
Kevin B <kberry@codeforamerica.org>
2016-02-02 17:33:21 -0800

    added cors support

--- api/__init__.py
+++ api/__init__.py
@@ -16,6 +16,15 @@ def create_app(config_filename):
     app.config.from_object(config_filename)
 
     api = Api(app)
+
+    @app.after_request
+    def after_request(response):
+      response.headers.add('Access-Control-Allow-Origin', '*')
+      response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+      response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+      return response
+
+      
     api.add_resource(FaxResource, '/fax')
     api.init_app(app)
 
